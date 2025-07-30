663b2c047a9111098fb56867dc6e07458a940d4d
yyeojung <hongshii0i0i@gmail.com>
2025-07-30 22:15:31 +0900

    model Product.js

---
+++ model/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = Schema(
+  {
+    sku: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: Array, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Object, required: true },
+    status: { type: String, default: "active" },
+    isDelete: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+productSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.__updateAt;
+  delete obj.__createAt;
+  return obj;
+};
+
+const Product = mongoose.modelNames("Product", productSchema);
+module.exports = Product;
