b53c75309a3fe2716b5abd5a0aa8318c6299c57e
yyeojung <hongshii0i0i@gmail.com>
2025-07-30 22:15:08 +0900

    app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // req.body가 객체로 인식
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on");
+});
