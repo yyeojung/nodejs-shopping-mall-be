fd5c40668244ed1b08ba6b667617f5ce29ee0d9d
yyeojung <hongshii0i0i@gmail.com>
2025-07-30 22:15:38 +0900

    model Cart.js

---
+++ model/Cart.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+
+const cartSchema = Schema(
+  {
+    userId: { type: mongoose.ObjectId, ref: User, required: true },
+    items: [
+      {
+        productId: { type: mongoose.ObjectId, ref: Product, required: true },
+        size: { type: String, required: true },
+        qty: { type: Number, default: 1, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+cartSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.__updateAt;
+  delete obj.__createAt;
+  return obj;
+};
+
+const Cart = mongoose.modelNames("Cart", cartSchema);
+module.exports = Cart;
