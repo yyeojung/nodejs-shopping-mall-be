eae0e6ca70271bf155edc24a30ccb68f9e09a60b
yyeojung <hongshii0i0i@gmail.com>
2025-08-03 00:24:55 +0900

    라우터 추가

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index");
 const app = express();
 
 require("dotenv").config();
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식
 
+app.use("/api", indexRouter);
+// /api/user
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
