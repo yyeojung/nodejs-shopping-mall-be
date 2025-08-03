1e3d62fc7744473d3f913433ea8420c9d5708740
yyeojung <hongshii0i0i@gmail.com>
2025-08-03 19:42:20 +0900

    /me 라우트에 인증 미들웨어 추가

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 
 router.post("/", userController.createUser); // 회원가입
 router.post("/login", userController.loginWithEmail); // 로그인
+router.get("/me", authController.authenticate, userController.getUser); // 토큰이 void한 토큰인지, 이 token가지고 유저를 찾아서 리턴
 
 module.exports = router;
