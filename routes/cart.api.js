0282b30e62451f66001c62a10c6b5efa96ce36c4
yyeojung <hongshii0i0i@gmail.com>
2025-08-10 15:34:06 +0900

    cart get, delete, update, getCartQty api 추가

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -4,5 +4,13 @@ const authController = require("../controllers/auth.controller");
 const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
+router.get("/", authController.authenticate, cartController.getCartList);
+router.delete(
+  "/:id",
+  authController.authenticate,
+  cartController.deleteCartItem
+);
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+router.get("/qty", authController.authenticate, cartController.getCartQty);
 
 module.exports = router;
