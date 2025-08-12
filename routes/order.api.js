71e8d8d5c2847be67a428777b71bbf4b37fceb9e
yyeojung <hongshii0i0i@gmail.com>
2025-08-12 22:32:54 +0900

    order 라우터

--- routes/order.api.js
+++ routes/order.api.js
@@ -4,5 +4,13 @@ const orderController = require("../controllers/order.controller");
 const router = express.Router();
 
 router.post("/", authController.authenticate, orderController.createOrder);
+router.get("/me", authController.authenticate, orderController.getOrder);
+router.get("/", authController.authenticate, orderController.getOrderList);
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  orderController.updateOrder
+);
 
 module.exports = router;
