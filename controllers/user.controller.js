322db60d92c248768cf08de0990a897c4a9ac56c
yyeojung <hongshii0i0i@gmail.com>
2025-08-17 00:13:21 +0900

    loginWithGoogle controller 추가

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const { OAuth2Client } = require("google-auth-library");
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 const userController = {};
 userController.createUser = async (req, res) => {
@@ -36,6 +38,41 @@ userController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+userController.loginWithGoogle = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    const { email, name } = ticket.getPayload();
+    let user = await User.findOne({ email });
+    if (!user) {
+      // 유저를 새로 생성
+      const randomPassword = "" + Math.floor(Math.random() * 10000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({
+        name,
+        email,
+        password: newPassword,
+      });
+      await user.save();
+    }
+
+    // 토큰 발행 리턴
+    const sessionToken = await user.generateToken();
+    res.status(200).json({
+      status: "success",
+      user,
+      token: sessionToken,
+    });
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
