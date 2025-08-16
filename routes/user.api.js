dd02dfd13aacd6cd4001964cc5d5fb84c4a3e381
yyeojung <hongshii0i0i@gmail.com>
2025-08-17 00:13:33 +0900

    구글 로그인 라우터 추가

--- routes/user.api.js
+++ routes/user.api.js
@@ -6,5 +6,6 @@ const authController = require("../controllers/auth.controller");
 router.post("/", userController.createUser); // 회원가입
 router.post("/login", userController.loginWithEmail); // 로그인
 router.get("/me", authController.authenticate, userController.getUser); // 토큰이 void한 토큰인지, 이 token가지고 유저를 찾아서 리턴
+router.post("/google", userController.loginWithGoogle); // 구글 로그인
 
 module.exports = router;
